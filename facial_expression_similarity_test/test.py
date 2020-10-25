f2625a1c1cc24396e4b29faccf5e339d43246203
Nathan21hz <842168590@qq.com>
2020-10-25 14:32:08 +0800

    update face recognition code

--- facial_expression_similarity_test/test.py
+++ facial_expression_similarity_test/test.py
@@ -1,10 +1,18 @@
 import dlib         
 import math 
-import cv2          
+import cv2
+import numpy as np  
 
-# Dlib 检测器和预测器
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v1.dat")
+
+
+def get_euclidean_distance(feature_1, feature_2):
+    feature_1 = np.array(feature_1)
+    feature_2 = np.array(feature_2)
+    dist = np.sqrt(np.sum(np.square(feature_1 - feature_2)))
+    return dist
 
 def cal_face_difference(image1,image2):
     img1_gray = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
@@ -13,62 +21,48 @@ def cal_face_difference(image1,image2):
     faces1 = detector(img1_gray, 0)
     faces2 = detector(img2_gray, 0)
 
-    points1 = predictor(image1, faces1[0]).parts()
-    points2 = predictor(image2, faces2[0]).parts()
+    points1 = predictor(image1, faces1[0])
+    points2 = predictor(image2, faces2[0])
 
-    DISTANCE_ERROR = 0
+    feature1 = facerec.compute_face_descriptor(image1, points1)
+    feature2 = facerec.compute_face_descriptor(image2, points2)
 
-    for i in range(len(points1)):
-        dist = math.sqrt((points1[i].x-points2[i].x)**2 + (points1[i].y-points2[i].y)**2)
-        DISTANCE_ERROR += dist
-    #print(DISTANCE_ERROR)
-    return DISTANCE_ERROR, DISTANCE_ERROR/len(points1)
+    dist = get_euclidean_distance(feature1,feature2)
+    return dist
 
-
-img1_rd = cv2.imread("test.png")
-
-img2_rd = cv2.imread("test.png")
 
 video1 = cv2.VideoCapture('01.mp4')
-video2 = cv2.VideoCapture('11.mp4')
+video2 = cv2.VideoCapture('01_copy.mp4')
 
 frame_count = 0
 total_frame_diff = 0
-total_point_diff = 0
 
 while True:
     ret1, frame1 = video1.read()
     ret2, frame2 = video2.read()
     if ret1 is False or ret2 is False:
         break
-    a, b = cal_face_difference(frame1,frame2)
-    total_frame_diff += a
-    total_point_diff += b
+    print(frame_count)
     frame_count += 1
-print("Average Frame Diff",total_frame_diff/frame_count,"\n","Average Point Diff",total_point_diff/frame_count)
+    total_frame_diff += cal_face_difference(frame1,frame2)
+print("Average",total_frame_diff/frame_count)
 
 
 '''
 01.mp4 - Driving Video
-01_copy.mp4 - Copy of Driving Video
+01_copy.mp4 - Copy of Driving Video - 0
 
-02.mp4 - another video containing facial expression
+02.mp4 - another video containing facial expression - 0.8356
 
-10.png - the first frame of Driving Video
+10.png - the first frame of Driving Video - 0.2417
 10.mp4 - Generated Video using 10.png
 
 11.png - a random frame of Driving Video
-11.mp4 - Generated Video using 11.png
+11.mp4 - Generated Video using 11.png - 0.2876
 
 20.png - another face photo
-20.mp4 - Generated Video using 20.png
+20.mp4 - Generated Video using 20.png - 0.8387
 
-Video1  Video2         Frame     Point
 
-01.mp4  01_copy.mp4      0.0       0.0
-01.mp4  02.mp4       2763.18     40.62
-01.mp4  10.mp4        108.64      1.60
-01.mp4  11.mp4        291.04      4.28
-01.mp4  20.mp4        383.53      5.64
-
+http://dlib.net/face_recognition.py.html
 '''
